338b0e39d2ef433de7afb845fe15657d74089fd2
Keiichi Takahashi <keiichi.takahashi@tel.com>
2022-10-17 09:38:43 +0900

    update

--- util_filter.py
+++ util_filter.py
@@ -21,19 +21,24 @@ class UtilFilter(AppObject):
         lab_step = LabelFrameNarrow('Step Selection')
         layout.addWidget(lab_step)
         # _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_
+        # filter for excluding step 1 (usually this step is just for stability)
+        but_exclude_step_1 = MenuButton('exclude Step 1')
+        but_exclude_step_1.clicked.connect(self.exclude_step_1)
+        layout.addWidget(but_exclude_step_1)
+        # _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_
         # filter for excluding step >= 1000 (de-chuck step)
         but_exclude_step_dechuck = MenuButton('exclude Step >= 1000')
         but_exclude_step_dechuck.clicked.connect(self.exclude_step_dechuck)
         layout.addWidget(but_exclude_step_dechuck)
+        # _____________________________________________________________________
+        # Condition Filters
+        lab_condition = LabelFrameNarrow('Condition Filter')
+        layout.addWidget(lab_condition)
         # _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_
         # filter for excluding steps without (setting data)
         but_exclude_no_setting = MenuButton('exclude Step w/o (setting data)')
         but_exclude_no_setting.clicked.connect(self.exclude_no_setting)
         layout.addWidget(but_exclude_no_setting)
-        # _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_
-        # Basic Filters for Column
-        lab_condition = LabelFrameNarrow('Filter with Condition')
-        layout.addWidget(lab_condition)
         # _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_
         # filter for excluding steps where (setting data) = 0
         but_exclude_setting_0 = MenuButton('exclude Step setting = 0')
@@ -45,17 +50,38 @@ class UtilFilter(AppObject):
         vpad.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Expanding)
         layout.addWidget(vpad)
 
+    def exclude_step_1(self):
+        but: QPushButton = self.sender()
+        flag = but.isChecked()
+        list_col = list()
+        model = self.getPanelSensorsModel()
+        # column search
+        for i in range(model.columnCount()):
+            name_head = model.headerData(i, Qt.Horizontal, Qt.DisplayRole)
+            if type(name_head) is not int:
+                continue
+            # condition
+            if name_head == 1:
+                list_col.append(i)
+        # set checkbox in the columns to flag
+        for col in list_col:
+            self.switch_check_all_rows(model, col, flag)
+
+        self.updateFeatures()
     def exclude_step_dechuck(self):
         but: QPushButton = self.sender()
         flag = but.isChecked()
         list_col = list()
         model = self.getPanelSensorsModel()
+        # column search
         for i in range(model.columnCount()):
             name_head = model.headerData(i, Qt.Horizontal, Qt.DisplayRole)
             if type(name_head) is not int:
                 continue
+            # condition
             if name_head >= 1000:
                 list_col.append(i)
+        # set checkbox in the columns to flag
         for col in list_col:
             self.switch_check_all_rows(model, col, flag)
 
