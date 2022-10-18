3b84000d8e129da12c1324033b9b9b59092f67ff
Keiichi Takahashi <keiichi.takahashi@tel.com>
2022-10-18 11:26:40 +0900

    update

--- package.ps1
+++ package.ps1
@@ -10,13 +10,13 @@ $workpath = @($basedir, $work) -join "\"
 $distpath = @($basedir, $dist) -join "\"
 # delete old workspace just in case
 if (Test-Path $specfile) {
-    Remove-Item -Path $specfile
+    Remove-Item -Path $specfile -Force
 }
 if (Test-Path $workpath) {
     Remove-Item -Path $workpath -Recurse -Force
 }
-if (Test-Path $workpath) {
+if (Test-Path $distpath) {
     Remove-Item -Path $distpath -Recurse -Force
 }
 # PyInstaller
-pyinstaller -w -i $icofile  --version-file app.rc --workpath $workpath --distpath $distpath -n $appname $mainscript
+pyinstaller -w -i $icofile --version-file app.rc --workpath $workpath --distpath $distpath -n $appname $mainscript
