3270766cd7d07dba022fc46bc62bdb5937958b8d
Keiichi Takahashi <keiichi.takahashi@tel.com>
2022-10-24 15:54:15 +0900

    update

--- app.py
+++ app.py
@@ -39,8 +39,8 @@ warnings.simplefilter('ignore', FutureWarning)
 class DCPCreator(QMainWindow):
     """DCP creator with the CSV file exported from the fleet analysis tool
     """
-    __version__ = '0.0.4'
-    __version_minor__ = '20221021'
+    __version__ = '0.0.5'
+    __version_minor__ = '20221024'
 
     # UI components
     console: LogConsole = None
